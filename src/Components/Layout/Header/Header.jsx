d2c0e01fee8e3009d5f815b3977286b1ed6d62b8
sebatian ruiz <sebasruiz909@gmail.com>
2022-03-10 17:28:37 -0500

    menu de navegacion responsive

--- src/Components/Layout/Header/Header.jsx
+++ src/Components/Layout/Header/Header.jsx
@@ -2,26 +2,41 @@ import React from 'react'
 import './Header.css';
 import { Logo } from '../../UI/Logo/Logo'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faMagnifyingGlass, faLongArrowAltRight } from '@fortawesome/free-solid-svg-icons'
+import { faMagnifyingGlass, faLongArrowAltRight , faBars } from '@fortawesome/free-solid-svg-icons'
 
 export const Header = () => {
   return (
     <div className='wrapper'>
         <nav>
+            <input type="checkbox"id='show-search'/> 
+            <input type="checkbox"id='show-menu'/> 
+            <label for="show-menu" className="menu-icon"><FontAwesomeIcon icon={ faBars }/></label>
+
             <div className="content">
             < Logo />
                 <ul className="links">
                     <li><p>Populares</p></li>
-                    <li><p>Categorias</p></li>
+                    <li>
+                        <p className='desktop-link' for="show-categorias">Categorias</p>
+                        <input type="checkbox" id='show-categorias'></input>
+                        <label className="label-text" for="show-categorias">Categorias</label>
+                        <ul >
+                            <li><p>Apartamentos</p></li>
+                            <li><p>Hogares</p></li>
+                            <li><p>Fincas</p></li>
+                            <li><p>En venta</p></li>
+                            <li><p>Arriendos</p></li>
+                        </ul>
+                    </li>
                     <li><p>Contacto</p></li>
                     <li><p>Ingresar</p></li>
-                    <li><p>Registar</p></li>   
+                    <li><p>Registrar</p></li>   
                 </ul>
             </div>
-            <div className="search-icon"><FontAwesomeIcon icon={faMagnifyingGlass}/></div>
+            <label for="show-search" className="search-icon"><FontAwesomeIcon icon={faMagnifyingGlass}/></label>
             <form action='#' className='search-box'>
-                <input type="text" placeholder="desear buscar algo..."></input>
-                <button className='go-icon'><FontAwesomeIcon icon={faLongArrowAltRight}></FontAwesomeIcon></button>
+                <input type="text" placeholder="desear buscar algo..." required></input>
+                <button type="submit" className='go-icon'><FontAwesomeIcon icon={faLongArrowAltRight}></FontAwesomeIcon></button>
             </form>
         </nav>
     </div>
