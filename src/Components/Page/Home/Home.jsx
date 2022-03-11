d4fac1b3e6f9869063f874cd06ed84206cb8333b
sebatian ruiz <sebasruiz909@gmail.com>
2022-03-11 08:55:47 -0500

    ventana home responsive terminada

--- src/Components/Page/Home/Home.jsx
+++ src/Components/Page/Home/Home.jsx
@@ -7,11 +7,15 @@ import './Home.css'
 
 
 export const Home = () => {
+  const URL = 'https://cdn.pixabay.com/photo/2017/04/06/02/46/bogota-2207075_960_720.jpg'
   return (
     <div>
         < Header />
-        <div className="home-container">
-           < HomeCover />
+        <div className="img-responsive-container">
+          <img className='img-responsive' src={ URL} alt="image" />
+          </div>
+        <div className="home-container">  
+           < HomeCover className="cover"></HomeCover>
            < HomeImgCover />
         </div>
     </div>
