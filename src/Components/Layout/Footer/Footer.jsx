d31a20a4ad06b61221af228a60266fde56406ed1
Karen Agudelo <kdagudelo70@misena.edu.co>
2022-03-22 08:01:41 -1200

    agregando estilo al footer

--- src/Components/Layout/Footer/Footer.jsx
+++ src/Components/Layout/Footer/Footer.jsx
@@ -1,9 +1,26 @@
+
+import './Footer.css'
 import { NavLink } from 'react-router-dom';
 export const Footer = ()=>{
     return(
         <>
-        <footer>
+        <footer className='footer'>
+            <div className='footer-container'>
 
+                <div>
+                    <NavLink className="navlink" to="/TerminosCondiciones"><li className='links-footer'>Términos y condiciones</li></NavLink> 
+                </div>
+
+                <div>
+                    <NavLink className="navlink" to="/PoliticasPrivacidad"><li className='links-footer'>Políticas de privacidad</li></NavLink> 
+                </div>
+
+                <div>
+                     <p>Síguenos</p>
+                </div>
+            </div>
+        <hr />
+        <p>	&copy; 2022  Esy House Rent Todos los derechos reservados</p>
         </footer>
         </>
     )
