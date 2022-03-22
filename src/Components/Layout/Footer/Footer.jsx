6b3fd612a044c7450acc6904dff4cb19e8e2a7c4
Karen Agudelo <kdagudelo70@misena.edu.co>
2022-03-22 10:05:41 -1200

    Terminado el footer

--- src/Components/Layout/Footer/Footer.jsx
+++ src/Components/Layout/Footer/Footer.jsx
@@ -1,4 +1,7 @@
-
+import {FaFacebookF} from 'react-icons/fa';
+import {FaInstagram} from 'react-icons/fa'
+import { FaTwitter} from 'react-icons/fa'
+import logoapp2 from '../../../img/logoapp2.ico'
 import './Footer.css'
 import { NavLink } from 'react-router-dom';
 export const Footer = ()=>{
@@ -6,6 +9,10 @@ export const Footer = ()=>{
         <>
         <footer className='footer'>
             <div className='footer-container'>
+                <div className='div1'>
+                    <img className='logo-app' src={logoapp2}    />
+                    <p className='under-logo'>¿Necesitas ayuda?</p><NavLink className="navlink" to=""><p className='links-writeus'>Escríbenos</p></NavLink>
+                </div>
 
                 <div>
                     <NavLink className="navlink" to="/TerminosCondiciones"><li className='links-footer'>Términos y condiciones</li></NavLink> 
@@ -16,11 +23,15 @@ export const Footer = ()=>{
                 </div>
 
                 <div>
-                     <p>Síguenos</p>
+                     <p className='followus'>Síguenos
+                     <li className='icons '><FaFacebookF color='#FFFF'/></li> 
+                     <li className='icons'><FaInstagram color='#FFFF'/></li>  
+                     <li className='icons' ><FaTwitter color='#FFFF'/></li></p>
                 </div>
             </div>
+            <br/>
         <hr />
-        <p>	&copy; 2022  Esy House Rent Todos los derechos reservados</p>
+        <p className='last-line'>	&copy; 2022  Easy House Rent Todos los derechos reservados</p>
         </footer>
         </>
     )
