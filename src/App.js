49052d15358410997b7463ac136eda18f6f4d3d5
sebatian ruiz <sebasruiz909@gmail.com>
2022-04-02 15:22:41 -0500

    funcionalidad rating con estrellas completa y funcional

--- src/App.js
+++ src/App.js
@@ -11,6 +11,10 @@ import { AboutUs } from "./Components/Page/AboutUs/AboutUs"
 import { ForgotPassword } from "./Components/Page/ForgotPassword/ForgotPassword";
 import { Profile } from "./Components/Page/Profile/Profile"
 import { Anouncement } from "./Components/Page/Anouncement/Anouncement";
+import  {StarRating}  from "./Components/UI/StarRating/StarRating";
+
+
+
 
 
 function App() {
@@ -18,7 +22,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={< StarRating/>} />
         <Route path="/Contact" element={<Contact />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
