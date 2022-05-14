bca38ead4ded336d4437ba6694ea98a48e93abc4
KarenAgvs <88803531+KarenAgvs@users.noreply.github.com>
2022-05-13 23:20:00 -0500

    Actualizacion en redireccion de header

--- src/Components/Page/AboutUs/AboutUs.jsx
+++ src/Components/Page/AboutUs/AboutUs.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import './AboutUs.css'
 import { Header } from '../../Layout/Header/Header'
 
 export const AboutUs = () => {
   return (
+    <>
     < Header />
+    <div className='title-content'>
+      {/* <img src='https://pixabay.com/es/photos/schnoor-el-barrio-de-schnoorviertel-3490494/' alt='background'/> */}
+      <h1 className='textTitle-nosotros'>Nosotros</h1>
+    </div> 
+    <div className='allcontainers'>
+      <div className='grid-div'></div>
+      <div className='grid-div' id='grid-item-photo'></div>
+      <div className='grid-div' id='grid-item-photo'></div>
+      <div className='grid-div' ></div>
+      <div className='grid-div'></div>
+      <div className='grid-div' id='grid-item-photo'></div>
+    </div >
+    <div className='menu-documentation'>
+      <a href="" className='link-documentation'>¿Cómo usarlo?</a>
+    </div>
+    </>
   )
 }
