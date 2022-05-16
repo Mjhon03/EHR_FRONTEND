f3fe16050e9ecfc998deca29b9de6fcddc3f46d3
sebasruiz09 <sebasruiz909@gmail.com>
2022-05-16 17:49:24 -0500

    envio de datos para renderizado de anuncio guiado por idanuncio

--- src/Components/ApiRoutes.js
+++ src/Components/ApiRoutes.js
@@ -3,3 +3,4 @@ export const urLDepartments = "https://localhost:44375/api/Department";
 export const urlMunicipality = "https://localhost:44375/api/Municipality/0?iddepartamento=";
 export const urlLogin = "https://localhost:44375/api/Login"
 export const urlSearch = "https://localhost:44375/api/home"
+export const urlAdversitement = 'https://localhost:44375/api/Advertisement/'
--- src/Components/Page/Anouncement/Anouncement.jsx
+++ src/Components/Page/Anouncement/Anouncement.jsx
@@ -1,33 +1,36 @@
 import axios from 'axios';
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { AnouncementInfo } from '../../Layout/AnoucementInfo/AnouncementInfo.jsx'
 import { Header } from '../../Layout/Header/Header'
 import { useSearchParams } from 'react-router-dom';
+import { urlAdversitement } from '../../ApiRoutes.js';
 
 export const Anouncement = () => {
-  
+
   const [searchParams] = useSearchParams();
   let idAnouncement = searchParams.get('idanounce');
-  let zone = searchParams.get('adzone') 
-  console.log(idAnouncement , zone );
 
-  const [ anouncementData , setAnoucementData ] = useState([])
+  const [anouncementData, setAnoucementData] = useState([])
 
-  const getDataAnouncement = () =>[
-    axios.get('urlAnouncements',{ params : { idanuncio : idAnouncement}})
-    .then(response => {
-      setAnoucementData(response.data)
-      console.log(response.data);
-    })
-    .catch(err => {
-      console.log(err);
-    })
-  ]
+  const getDataAnouncement = () => {
+    axios.get(`${urlAdversitement}${idAnouncement}`)
+      .then(response => {
+        setAnoucementData(response.data)
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  }
+
+  useEffect(() => {
+    getDataAnouncement()
+  },[])
+
 
   return (
     <div>
-        <Header />
-        <AnouncementInfo  data={ anouncementData}/>
+      <Header />
+      <AnouncementInfo data={anouncementData} />
     </div>
   )
 }
