214537d25b0dd5c36c78c588bbc53ad2270ab823
sebasruiz09 <sebasruiz909@gmail.com>
2022-06-06 22:07:58 -0500

    desarrollo responsive del componente home-section

--- src/Components/Page/PreviewPage/PreviewPage.jsx
+++ src/Components/Page/PreviewPage/PreviewPage.jsx
@@ -1,11 +1,12 @@
 import { Header } from "../../Layout/Header/Header"
 import { UserImage } from "../../UI/UserImage/UserImage"
+import { HomeSection } from '../../Layout/HomeSection/HomeSection'
 
 
 export const PreviewPage = () => {
   return (
     <>
-      <Header />
+      <HomeSection />
     </>
   )
 }
