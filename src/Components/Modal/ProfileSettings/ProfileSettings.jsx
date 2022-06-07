3ba773f05d9ba86a1082e6258886baf33d2aa420
sebasruiz09 <sebasruiz909@gmail.com>
2022-06-07 17:36:02 -0500

    responsive de funcionalidades de menu y funcionalidad de cambio de estado con clic por fuera

--- src/Components/Modal/ProfileSettings/ProfileSettings.jsx
+++ src/Components/Modal/ProfileSettings/ProfileSettings.jsx
@@ -1,7 +1,7 @@
 import { faRocketchat } from '@fortawesome/free-brands-svg-icons'
 import { faCaretDown, faCircleInfo, faGears, faIdCardClip } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import React,{useState} from 'react'
+import React, { useState } from 'react'
 import { NavLink, useNavigate } from 'react-router-dom'
 import { ProfileAction, ProfileActionText, ProfileModal, SettingsSpace } from '../../StyledComponents/Overlay/StyledComponents'
 import './ProfileSettings.css'
@@ -9,20 +9,24 @@ import './ProfileSettings.css'
 
 export const ProfileSettings = ({ userData }) => {
 
-  const [ settingsVisibility , setSettingsVisibility ] = useState(false)
+  const [settingsVisibility, setSettingsVisibility] = useState(false)
 
   const navigate = useNavigate()
 
-  const changeSettings = () =>{
-    if(settingsVisibility === false){
+  const changeSettings = () => {
+    if (settingsVisibility === false) {
       setSettingsVisibility(true)
-    }else{
+    } else {
       setSettingsVisibility(false)
     }
   }
 
+  const changeVisbilility = () => {
 
-  const deleteSesion = () =>{
+  }
+
+
+  const deleteSesion = () => {
     localStorage.removeItem("userInfo")
     localStorage.setItem("userInfo", null)
     navigate('/')
@@ -32,34 +36,36 @@ export const ProfileSettings = ({ userData }) => {
 
   return (
     <>
-    <button className='profile-modal-activate'  onClick={()=> changeSettings()}><FontAwesomeIcon className='activate-profile-icon' icon={ faCaretDown} /></button>
-    {settingsVisibility &&
-      <ProfileModal>
-        <NavLink to='/profile'>
-        <div className="settings-basic-info">
-          <div className="settings-image-container">
-            <img src={ userData[0].foto} alt="settingsImage" className='settings-image' />
-          </div>
-          
-            <div className="settings-profile">
-              <p className='settings-profile-name'>{ userData[0].nombre}</p>
-              <p className='send-profile-button'>Ver tu perfil</p>
+      <button className='profile-modal-activate' onClick={() => changeSettings()}><FontAwesomeIcon className='activate-profile-icon' icon={faCaretDown} /></button>
+      {settingsVisibility &&
+        <div className="status" onClick={changeSettings}>
+          <ProfileModal>
+            <NavLink to='/profile'>
+              <div className="settings-basic-info">
+                <div className="settings-image-container">
+                  <img src={userData[0].foto} alt="settingsImage" className='settings-image' />
+                </div>
+                <div className="settings-profile">
+                  <p className='settings-profile-name'>{userData[0].nombre}</p>
+                  <p className='send-profile-button'>Ver tu perfil</p>
+                </div>
+              </div>
+            </NavLink>
+            <SettingsSpace />
+            <div className="settings-container">
+              <ProfileAction><FontAwesomeIcon className='settings-action-icon' icon={faRocketchat} /><ProfileActionText>Mensajes</ProfileActionText></ProfileAction>
+              <ProfileAction><FontAwesomeIcon className='settings-action-icon' icon={faGears} /><ProfileActionText>Configuracion</ProfileActionText></ProfileAction>
+              <ProfileAction><FontAwesomeIcon className='settings-action-icon' icon={faCircleInfo} /><ProfileActionText>Ayuda y soporte</ProfileActionText></ProfileAction>
+              <ProfileAction><FontAwesomeIcon className='settings-action-icon' icon={faIdCardClip} /><ProfileActionText>Contacto</ProfileActionText></ProfileAction>
             </div>
+            <SettingsSpace />
+            <div className="settings-action-container">
+              <button className='settings-close' onClick={deleteSesion} >Cerrar sesion</button>
+            </div>
+          </ProfileModal>
         </div>
-        </NavLink>
-        <SettingsSpace />
-        <div className="settings-container">
-          <ProfileAction><FontAwesomeIcon className='settings-action-icon' icon={ faRocketchat }/><ProfileActionText>Mensajes</ProfileActionText></ProfileAction>
-          <ProfileAction><FontAwesomeIcon className='settings-action-icon' icon={ faGears } /><ProfileActionText>Configuracion</ProfileActionText></ProfileAction>
-          <ProfileAction><FontAwesomeIcon className='settings-action-icon' icon={ faCircleInfo }/><ProfileActionText>Ayuda y soporte</ProfileActionText></ProfileAction>
-          <ProfileAction><FontAwesomeIcon className='settings-action-icon' icon={ faIdCardClip }/><ProfileActionText>Contacto</ProfileActionText></ProfileAction>
-        </div>
-        <SettingsSpace />
-          <div className="settings-action-container">
-            <button className='settings-close' onClick={ deleteSesion } >Cerrar sesion</button>
-          </div>
-        </ProfileModal>
       }
+
     </>
   )
 }
--- src/Components/Page/PreviewPage/PreviewPage.jsx
+++ src/Components/Page/PreviewPage/PreviewPage.jsx
@@ -1,12 +1,6 @@
-import { Header } from '../../Layout/Header/Header'
-import { HeaderPrueba } from '../../Layout/Header/Header'
-import { RegisterFooter } from '../../Layout/RegisterFooter/RegisterFooter'
-import { RandomBanner } from '../RandomBanner/RandomBanner'
-
 export const PreviewPage = () => {
   return (
     <>
-      <Header />
     </>
   )
 }
