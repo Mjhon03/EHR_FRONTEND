4a132dfefc133d552c41b3fb197b42c1bc7080e4
sebasruiz09 <sebasruiz909@gmail.com>
2022-06-07 11:01:18 -0500

    configuracion de nuevo header con diseño responsive

--- src/Components/Layout/Header/Header.jsx
+++ src/Components/Layout/Header/Header.jsx
@@ -10,103 +10,78 @@ import { useNavigate } from 'react-router'
 
 export const Header = () => {
 
-    const [userSession, setUserSession] = useState(0)
+  const [userSession, setUserSession] = useState(0)
 
-    const { user } = useContext(UserContext)
+  const { user } = useContext(UserContext)
 
-    const SetUserData = () => {
-        if (user != null) {
-            setUserSession(1)
-        } else {
-            setUserSession(0)
-        }
+  const SetUserData = () => {
+    if (user != null) {
+      setUserSession(1)
+    } else {
+      setUserSession(0)
     }
+  }
 
-    useEffect(() => {
-        SetUserData()
-    })
+  useEffect(() => {
+    SetUserData()
+  })
 
-    const navigate = useNavigate()
+  const navigate = useNavigate()
 
-    const sendHome = () => {
-        navigate('/')
-    }
+  const sendHome = () => {
+    navigate('/')
+  }
 
 
-    return (
-        <div className="header-container">
-            <div className="header-logo-container">
-                <img onClick={sendHome} src="https://i.ibb.co/zQHyDyt/logo.png" alt="logoEhr" className='logo-header' />
-                <p onClick={sendHome} className='header-logo'>EHR</p>
-            </div>
-            <div className="header-actions">
+  return (
+    <div className="header-container">
+      <div className="header-logo-container">
+        <div className="logo-content">
+          <img src="https://i.ibb.co/zQHyDyt/logo.png" alt="logoEhr" className='logo-header' />
+        </div>
+        <p className='header-logo'>EHR</p>
+      </div>
+      <div className="header-actions">
 
-                <div className='iconHeader'>
-                    <NavLink to='/'>
-                        <button className='header-action-button '><FontAwesomeIcon className='header-action-icon' icon={faHouseChimneyUser} /></button>
-                        <div className='viewTextIcon'><p>Inicio</p></div>
-                    </NavLink>
-                </div>
-
-                <div className='iconHeader'>
-                    <NavLink to='/'>
-                        <button className='header-action-button'><FontAwesomeIcon className='header-action-icon' icon={faFilterCircleDollar} /></button>
-                        <div className='viewTextIcon'><p>Recientes</p></div>
-                    </NavLink>
-                </div>
-
-                <div className='iconHeader'>
-                    <NavLink to='/AboutUs'>
-                        <button className='header-action-button'><FontAwesomeIcon className='header-action-icon' icon={faCircleInfo} /></button>
-                        <div className='viewTextIcon'><p>Nosotros</p></div>
-                    </NavLink>
-                </div>
-
-                <div className='iconHeader'>
-                    <NavLink to='/'>
-                        <button className='header-action-button'><FontAwesomeIcon className='header-action-icon' icon={faCommentDots} /></button>
-                        <div className='viewTextIcon'><p>Chat</p></div>
-                    </NavLink>
-                </div>
-
-                <div className='iconHeader'>
-                    <NavLink to='/support'>
-                        <button className='header-action-button action-responsive'><FontAwesomeIcon className='header-action-icon' icon={faNewspaper} /></button>
-                        <div className='viewTextIcon'><p>Soporte</p></div>
-                    </NavLink>
-                </div>
-
-                <div className='iconHeader'>
-                    <NavLink to='/'>
-                        <button className='header-action-button action-responsive'><FontAwesomeIcon className='header-action-icon' icon={faHeadset} /></button>
-                        <div className='viewTextIcon'><p>Contacto</p></div>
-                    </NavLink>
-                </div>
-
-            </div>
-
-            {userSession === 0 &&
-                <div className="header-nouser-actions">
-                    <NavLink to='/login'><button className='nouser-action'>Ingresar</button></NavLink>
-                    <NavLink to='/register'><button className='nouser-action'>Registrarme</button></NavLink>
-                    <div className="header-action-button header-responsive">
-                        <FontAwesomeIcon className='header-action-icon' icon={faAngleDown} />
-                    </div>
-                </div>
-            }
-            {userSession === 1 &&
-                <div className="header-nouser-actions">
-                    <NavLink to="/Profile">
-                        <div className="header-user-settings">
-                            <img src={user[0].foto} className='header-profile' alt="header-profile" />
-                            <p className='text-header-profile'>{user[0].nombre}</p>
-                        </div>
-                    </NavLink>
-                    <ProfileSettings userData={user} />
-                </div>
-            }
+        <div className='iconHeader'>
+          <NavLink to='/'>
+            <button className='header-action-button '><FontAwesomeIcon className='header-action-icon' icon={faHouseChimneyUser} /></button>
+          </NavLink>
         </div>
 
-
-    )
+        <div className='iconHeader'>
+          <NavLink to='/'>
+            <button className='header-action-button'><FontAwesomeIcon className='header-action-icon' icon={faFilterCircleDollar} /></button>
+          </NavLink>
+        </div>
+        <div className='iconHeader'>
+          <NavLink to='/AboutUs'>
+            <button className='header-action-button'><FontAwesomeIcon className='header-action-icon' icon={faCircleInfo} /></button>
+          </NavLink>
+        </div>
+        <div className='iconHeader'>
+          <NavLink to='/'>
+            <button className='header-action-button'><FontAwesomeIcon className='header-action-icon' icon={faCommentDots} /></button>
+          </NavLink>
+        </div>
+        <div className='iconHeader'>
+          <NavLink to='/support'>
+            <button className='header-action-button action-responsive'><FontAwesomeIcon className='header-action-icon' icon={faNewspaper} /></button>
+          </NavLink>
+        </div>
+        <div className='iconHeader'>
+          <NavLink to='/'>
+            <button className='header-action-button action-responsive'><FontAwesomeIcon className='header-action-icon' icon={faHeadset} /></button>
+          </NavLink>
+        </div>
+      </div>
+      {userSession === 0 &&
+        <div className="nouser-actions">
+          <NavLink to='/login'className='nouser-action'><button >Ingresar</button></NavLink>
+          <NavLink to='/register'className='nouser-action'><button >Registrarme</button></NavLink>
+          <FontAwesomeIcon className='icon-none header-action-button' icon={faAngleDown} />       
+        </div>
+      }
+    </div>
+  )
 }
--- src/Components/Page/PreviewPage/PreviewPage.jsx
+++ src/Components/Page/PreviewPage/PreviewPage.jsx
@@ -1,11 +1,12 @@
 import { Header } from '../../Layout/Header/Header'
+import { HeaderPrueba } from '../../Layout/Header/Header'
 import { RegisterFooter } from '../../Layout/RegisterFooter/RegisterFooter'
 import { RandomBanner } from '../RandomBanner/RandomBanner'
 
 export const PreviewPage = () => {
   return (
     <>
-      <Header/>
+      <Header />
     </>
   )
 }
