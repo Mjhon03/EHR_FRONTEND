bdec33900254afc938d43b80c537a78e9d1aa9a3
sebasruiz09 <sebasruiz909@gmail.com>
2022-06-28 00:00:05 -0500

    solucion de errores en anuncio y cambio de iconos de la aplicacion

--- src/Components/Page/AboutUs/AboutUs.jsx
+++ src/Components/Page/AboutUs/AboutUs.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import { Footer } from '../../Layout/Footer/Footer'
 import { Header } from '../../Layout/Header/Header'
+import './AboutUs.css'
 
 export const AboutUs = () => {
   return (
     <>
       <Header />
+      <div className="banner-about-container">
+        <div className="banner-about-title">
+
+        </div>
+      </div>
       <Footer />
     </>
   )
