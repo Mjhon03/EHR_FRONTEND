7eb6f051f14c03b569051e5befa25caaaf8f0aba
sebasruiz09 <sebasruiz909@gmail.com>
2022-06-29 17:44:38 -0500

    avance en diseño de chat y render de datos

--- src/Components/Page/Chat/Chat.jsx
+++ src/Components/Page/Chat/Chat.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { useState } from 'react'
 import { db } from '../../../firebase'
 import { UseChat } from '../../../UseChat'
@@ -7,10 +7,23 @@ import { Header } from '../../Layout/Header/Header'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPaperPlane } from '@fortawesome/free-solid-svg-icons'
 import { useEffect } from 'react'
+import { UserContext } from '../../../UserProvider/UserProvider'
+import { useNavigate } from 'react-router'
 
 export const Chat = () => {
 
-    let date = new Date()
+    const { user } = useContext(UserContext)
+
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        if (user === null) {
+            navigate('/')
+        }
+    })
+
+    let currentTime = new Date() 
+    let date = currentTime.toLocaleTimeString()
 
     const [message, setMessage] = useState('')
     const { loading, messages, error } = UseChat()
@@ -18,15 +31,17 @@ export const Chat = () => {
     const onPress = (e) => {
         db.collection('messages').add({
             timestamp: Date.now(),
-            date,
-            message
+            date : date,
+            message,
+            user: user[0].idusuario,
+            photo: user[0].foto
         })
         changeDefault()
     }
 
-     const sendEvent =(event)=>{
+    const sendEvent = (event) => {
         let charCode = event.keyCode;
-        if (charCode===13){
+        if (charCode === 13) {
             onPress()
         }
     }
@@ -34,6 +49,7 @@ export const Chat = () => {
     const changeDefault = () => {
         setMessage('')
     }
+
 
     return (
         <div className='chat-box'>
@@ -44,14 +60,42 @@ export const Chat = () => {
                 </div>
                 <div className="chat-form-content">
                     <div className="messages-container">
-                       <ul>
-                        {messages.map(m => <li key={m.id}>{m.message}</li>)}
-                        </ul> 
+                        {messages.map(m => {
+                            if (m.user === user[0].idusuario) {
+                                return (
+                                    <div className="message-content">
+                                        <div className="photo-content-msj">
+                                            <img src={m.photo} alt="profile" className='photo-msj' />
+                                        </div>
+                                        <div className="message-container-text">
+                                            <p className='send-message' key={m.id}>{m.message}{m.date}</p>
+                                        
+                                        </div>
+                                    </div>
+                                )
+                            }
+                            else {
+                                return (
+                                    <div className="messages-content">
+                                        <p className='send-message' key={m.id}>{m.message}</p>
+                                        <div className="photo-content-msj">
+                                            {
+                                                m.photo === ' ' ?
+                                                    <img src='https://cdn.pixabay.com/photo/2017/11/10/05/48/user-2935527_960_720.png' alt="" />
+                                                    :
+                                                    <img src={m.photo} alt="profile" className='photo-msj' />
+                                            }
+                                        </div>
+                                    </div>
+                                )
+                            }
+                        }
+
+                        )}
                     </div>
-                    
                     <div className='send-message-content'>
-                        <input autoFocus className='info-input-send' placeholder='Escribe un mensaje aquí' onKeyUp={sendEvent}  value={message} type='text' onChange={(e) => setMessage(e.target.value)} />
-                        <FontAwesomeIcon className='send-message-icon' icon={faPaperPlane}  onClick={ onPress} />
+                        <input autoFocus className='info-input-send' placeholder='Escribe un mensaje aquí' onKeyUp={sendEvent} value={message} type='text' onChange={(e) => setMessage(e.target.value)} />
+                        <FontAwesomeIcon className='send-message-icon' icon={faPaperPlane} onClick={onPress} />
                     </div>
                 </div>
             </div>
