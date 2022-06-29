2e83bf7496de3c8e5dc7fc93d458bf3966248c3b
sebasruiz09 <sebasruiz909@gmail.com>
2022-06-28 22:56:16 -0500

    diseño responsive terminado de ventana sobre nosotros

--- src/Components/Page/AboutUs/AboutUs.jsx
+++ src/Components/Page/AboutUs/AboutUs.jsx
@@ -12,7 +12,7 @@ export const AboutUs = () => {
       <div className="banner-about-container">
         <div className="banner-about-title">
           <h1>EASY HOUSE RENT</h1>
-          <p></p>
+          <p className='banner-about-text'>Informacion sobre nosotros y el desarrollo del aplicativo</p>
         </div>
       </div>
       <div className="about-slogan">
@@ -20,8 +20,8 @@ export const AboutUs = () => {
           <img className='slogan-img' src="https://cdni.iconscout.com/illustration/premium/thumb/boy-making-decision-with-help-of-discussion-2527773-2114679.png" alt="group" />
         </div>
         <div className="text-slogan-container">
-          <h2>Nuestra idea</h2>
-          <p>Esta nace de generar facilidad al usuario en la tarea de emprender la busqueda de un inmueble, con Easy House Rent el ezfuerzo se minimiza obteniendo diversas opciones en cuestion de segundos , eligiendo la de tu preferencia y contactando directamente con su propietario</p>
+          <h2 className='slogan-title'>Nuestra idea</h2>
+          <p className='slogan-text'>Esta nace de generar facilidad al usuario en la tarea de emprender la busqueda de un inmueble, con Easy House Rent el ezfuerzo se minimiza obteniendo diversas opciones en cuestion de segundos , eligiendo la de tu preferencia y contactando directamente con su propietario</p>
         </div>
       </div>
       <div className="tags-container">
@@ -53,7 +53,7 @@ export const AboutUs = () => {
           <FontAwesomeIcon icon={faTractor} className='icon-about' />
           <div className="line-about" />
           <div className="tags-text-content">
-            <h2 className='tag-title'></h2>
+            <h2 className='tag-title'>Sectores</h2>
             <p className='tag-tile'>En easy house rent optamos porque el usuario encuentre mas opciones a su alcance, por eso    </p>
           </div>
         </div>
@@ -61,16 +61,16 @@ export const AboutUs = () => {
           <FontAwesomeIcon icon={faCheck} className='icon-about' />
           <div className="line-about" />
           <div className="tags-text-content">
-            <h2 className='tag-title'></h2>
-            <p className='tag-tile'></p>
+            <h2 className='tag-title'>Responsabilidad</h2>
+            <p className='tag-tile'>Nuestro equipo de trabajo a logrado cumplir con cada tarea propuesta para lograr el objetivo</p>
           </div>
         </div>
         <div className="tags-content">
           <FontAwesomeIcon icon={faShieldHalved} className='icon-about' />
           <div className="line-about" />
           <div className="tags-text-content">
-            <h2 className='tag-title'></h2>
-            <p className='tag-tile'></p>
+            <h2 className='tag-title'>Seguridad</h2>
+            <p className='tag-tile'>Hemos logrado implementar certificados de seguridad y transferencia de datos para un sitio mas seguro</p>
           </div>
         </div>
       </div>
